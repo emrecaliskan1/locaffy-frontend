daf5fc1c0872bb1040d1878aa1d6b3a6ae9ae48d
Emre Çalışkan <114865537+emrecaliskan1@users.noreply.github.com>
2025-12-04 15:54:49 +0300

    fix2

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -66,7 +66,7 @@ export default function HomeScreen({ navigation }) {
     useCallback(() => {
       loadFavorites();
       loadPlaces();
-    }, []) 
+    }, [appliedFilters, currentLocation]) 
   );
 
   const loadPlaces = async () => {
